fb7c4a368864d30a8551881c3c9d000830175aae
Glitch (spiny-reinvented-society) <none>
2021-01-27 22:21:40 +0000

    🎆🌧 Updated with Glitch

---
+++ emprator.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const { Client, RichEmbed } = require('discord.js');
+const app = express();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+const http = require('http');
+app.get("/", (request, response) => {
+  console.log(Date.now() + "hostemprator");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000) 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const data = new Map();
+
+client.login(process.env.token);
+
+client.on("ready", () => {
+  console.log(
+    `Online In Servers : ${client.guilds.size} | Users : ${client.users.size}`
+  ); ///By Emprator
+  let statuses = [
+    `RE UP`,
+    `RE UP`
+  ];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    client.user.setActivity(STREAMING, {
+      type: "PLAYING",
+      url: "https://www.twitch.tv/faith"
+    });
+  }, 3000);
+});
+/// REKLAM ///
+client.on('message' , message  => {
+  if (message.content.startsWith("wara")) {
+ 
+var join = client.channels.get("801921158588530710")/// ئاید ڤۆیس
+if (join) join.join()
+ 
+  }
+})
+ 
+ 
+ 
+client.on("message" , message => {
+ 
+if(message.channel.type === 'dm') {
+ 
+if (message.content.startsWith("https://discord.gg/")) {
+ 
+  message.author.send(`
+https://discord.gg/WggjWQ9E9N`)/// لینکە سیرڤەرە خۆت
+ 
+  client.channels.get("801922461385621545").send(
+`> Nerdra la layan <@${message.author.id}> 
+${message.content}`)
+}
+    }
+        })
