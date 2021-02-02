cc842a23a8f7b49f39ac55676893e93454a3f634
sanranj87 <74021060+sanranj87@users.noreply.github.com>
2021-02-03 00:33:56 +0300

    Create reroll.js

---
+++ commands/Giveaways/reroll.js
@@ -0,0 +1,32 @@
+exports.run = async (client, message, args, lang) => {
+    let role = client.db.fetch(`role_${message.guild.id}`);
+    if (!role) role = client.config.grole;
+
+    // If the member doesn't have enough permissions
+    if (!message.member.hasPermission("MANAGE_GUILD") && !message.member.roles.cache.some((r) => r.name === role))return message.channel.send(lang.start.perms + "** **" + "`" + role + "`" + ".");
+
+    
+    // If no message ID or giveaway name is specified
+    if (!args[0]) return message.channel.send(lang.reroll.msg);
+
+    if(message.member.guild.id != client.giveawaysManager.giveaways.find((g) => g.messageID === args[0]).guildID) return message.channel.send(lang.delete.otherServer);
+    if("<@" + message.member.id + ">" != client.giveawaysManager.giveaways.find((g) => g.messageID === args[0]).hostedBy) return message.channel.send(lang.delete.otherUser);
+
+    // try to found the giveaway with prize then with ID
+    let giveaway = client.giveawaysManager.giveaways.find((g) => g.prize === args.join(' ')) || client.giveawaysManager.giveaways.find((g) => g.messageID === args[0]);
+
+    // If no giveaway was found
+    if (!giveaway) return message.channel.send(lang.reroll.err + "** **" + "`" + args.join(' ') + "`" + ".");
+    
+
+    let messageID = args[0];
+
+    client.giveawaysManager.reroll(messageID, {
+        messages: {
+            congrat: lang.reroll.good,
+            error: lang.reroll.parts
+        }
+    }).catch((err) => {
+        message.channel.send(lang.reroll.err + "** **" + "`" + messageID + "`" + ".");
+    });
+};
