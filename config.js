ad019da9fa1ab0a87e97f5bd4adaa1c22d600209
sanranj87 <74021060+sanranj87@users.noreply.github.com>
2021-02-03 00:05:25 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {	
+hostedBy: true, 
+prefix: "g!",
+OwnerID: "722210496295600242", 
+token: "Nzk1MzY3NzI5Mjg0NjQ0OTQ0.X_IV6w.btRhERgfuBDCa6_PIh9J40gtRVo",
+support: "https://discord.gg/FMreW7BwNk",
+basiclang: "en",
+color: "RED",
+reaction: "🎉", 
+grole: "Giveaway Management", 
+
+}
