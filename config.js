0087b66e124c487412cb204ea1b59c340702fde6
sanranj87 <74021060+sanranj87@users.noreply.github.com>
2021-02-04 01:09:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 hostedBy: true, 
 prefix: "g!",
 OwnerID: "722210496295600242", 
-token: "Nzk1MzY3NzI5Mjg0NjQ0OTQ0.X_IV6w.btRhERgfuBDCa6_PIh9J40gtRVo",
+token: "Nzk1MzY3NzI5Mjg0NjQ0OTQ0.X_IV6w.YgI3iomaLTaXU1PhOBuCIZ5vkhs",
 support: "https://discord.gg/FMreW7BwNk",
 basiclang: "en",
 color: "RED",
