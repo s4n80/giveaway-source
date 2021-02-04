92a6633517d56d93e8f20354153506ff6c0be09f
sanranj87 <74021060+sanranj87@users.noreply.github.com>
2021-02-04 22:44:43 +0300

    Update help.js

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -3,13 +3,14 @@ exports.run = async (client, message, args, lang) => {
     let version = require("../../package.json").version;
     let discord_giveaways = require("../../package.json").dependencies["discord-giveaways"];
     let supportURL = client.config.support;
+    let website = `https://giveawayybot.glitch.me/`
     let invite = `https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`;
     message.channel.send(new MessageEmbed()
         .setTitle(lang.help.title)
         .addField(lang.help.giveawaycmd,'`create`, `start`, `end`, `edit`, `reroll`, `delete`')
         .addField(lang.help.featuredcmd,'`setlang`, `set logs`, `set mention on`, `set mention off`, `setprefix`, `set role`')
         .addField(lang.help.infocmd,'`stats`, `ping`, `invite`, `help`')
-        .addField(lang.help.link, `[Support server](${supportURL}) | [Invite](${invite})`)
+        .addField(lang.help.link, `[Support server](${supportURL}) | [Invite](${invite}) | [Website](${website}) `)
         .setColor("RED"))
 }
           
