472c03c225fd4c6441b6a4e974057365cc4975b9
sanranj87 <74021060+sanranj87@users.noreply.github.com>
2021-03-17 14:35:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {	
 hostedBy: true, 
-prefix: "g!",
-OwnerID: "722210496295600242", 
-token: "Nzk1MzY3NzI5Mjg0NjQ0OTQ0.X_IV6w.YgI3iomaLTaXU1PhOBuCIZ5vkhs",
-support: "https://discord.gg/FMreW7BwNk",
+prefix: "",
+OwnerID: "", 
+token: "",
+support: "",
 basiclang: "en",
-color: "RED",
+color: "",
 reaction: "🎉", 
 grole: "Giveaway Management", 
 
